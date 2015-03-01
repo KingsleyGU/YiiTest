4cd8fb95af7692cf430404b7095798a79308b9cf
Gu Min <gumin12150454x@gmail.com>
2015-03-01 21:26:55 +0100

    test

--- protected/controllers/CommentsController.php
+++ protected/controllers/CommentsController.php
@@ -2,11 +2,13 @@
 
 class CommentsController extends Controller
 {
+		
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
 	public $layout='//layouts/column2';
+	$hello = '';
 
 	/**
 	 * @return array action filters
